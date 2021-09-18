2f28741029cbe0e697c6014b40666a681c7e10fa
SubhraJs <mistusaha316@gmail.com>
2021-09-19 01:25:32 +0530

    first commit

---
+++ training day 4/add with java.js
@@ -0,0 +1,16 @@
+var number1 = 1;
+    number2 = 2;
+
+    
+    var choice = 3 ;
+    switch (choice){
+        case 0:
+            console.log(day1);
+            break;
+        case 1:
+            console.log(day2);
+            break;
+            default:
+                console.log("hi i am default");
+                break;
+    }
---
+++ training day 4/for loop.js
@@ -0,0 +1,15 @@
+var printStar = ""
+for(var counter=1;counter<=5;counter++)
+		{
+			for(var index=1;index<=5;index++){
+                if(counter==1 || counter==5 || index==1 || index==5){
+                    printStar=printStar+"* "
+                }
+                else{
+                    printStar
+				
+			}
+        }
+		printStar=printStar+"\n";
+        }
+    console.log(printStar);
